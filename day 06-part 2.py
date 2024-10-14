a4117ab09bc09d157f5d99a09c8c554fc600a536
node62 <theoregi61@gmail.com>
2024-10-14 16:58:01 +0530

    day6 part2

---
+++ day 06-part 2.py
@@ -0,0 +1,19 @@
+print("Enter the times and distances lines:")
+
+inp_times = input() 
+inp_distances = input()  
+
+# Parse input in a less clean way
+times = [int("".join(inp_times.split(":")[1].split()))]
+distances = [int("".join(inp_distances.split(":")[1].split()))]
+
+result = 1
+
+for t, d in zip(times, distances):
+    counter = 0
+    for h in range(t):
+        if h * (t - h) > d:
+            counter += 1
+    result *= counter
+
+print(result)
